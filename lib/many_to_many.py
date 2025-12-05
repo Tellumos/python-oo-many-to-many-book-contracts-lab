0d2721fed87f5c27ef5e843633997d620b28d9af
mvrz <msv930@gmail.com>
2025-12-05 17:41:12 -0600

    contracts_by_date correctly returns list of matching dates

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,58 @@
 class Author:
-    pass
+    all = []
+    def __init__(self, name):
+        self.name = name
 
+    def contracts(self):
+        return [contracts for contracts in Contract.all if contracts.author == self]
+    
+    def books(self):
+        return [contracts.book for contracts in Contract.all if contracts.author == self]
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+    
+    def total_royalties(self):
+        sum = 0
+        for royalty in [contracts.royalties for contracts in Contract.all if contracts.author == self]:
+            sum += royalty
+        return sum
 
 class Book:
-    pass
+    all = []
+    def __init__(self, title):
+        self.title = title
+
+    def contracts(self):
+        return [contracts for contracts in Contract.all if contracts.book == self]
+    
+    def authors(self):
+        return [contracts.author for contracts in Contract.all if contracts.book == self]
 
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        if isinstance(author, Author):
+            self.author = author
+        else:
+            raise ValueError("Not an instance of Author class")
+        if isinstance(book, Book):
+            self.book = book
+        else:
+            raise ValueError("Not an instance of Book class")
+        if isinstance(date, str):
+            self.date = date
+        else:
+            raise ValueError("Not a string")
+        if isinstance(royalties, int):
+            self.royalties = royalties
+        else:
+            raise ValueError("Not an integer")
+        
+        Contract.all.append(self)
+     
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contracts for contracts in cls.all if contracts.date == date]
